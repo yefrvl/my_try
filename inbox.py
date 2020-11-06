5a3e620c4959b57e12ef2587b04933b1339209ef
VAE <vlad.efr92@yandex.ru>
2020-11-06 11:49:35 +0300

    рисует 5 цифр вразнобой рисует двойку полностью и идет по двойке вверх для перехода на тройку

--- inbox.py
+++ inbox.py
@@ -2,16 +2,16 @@ import turtle
 from math import sqrt
 
 loops = 0
-x = 30
-y = 50
+x = 45
+y = 0
 
 turtle.shape('turtle')
 dlina = 30
 null = [0, 180, 90, 0, 90, 90, 0]
 one = [45, -135, 0, 180, 0]
-two = [0, -90, -45, 135]
+two = [0, -90, -45, 135, 180, -135, 45]
 three = [0, -135, 135, -135]
-four = [-90, 90, -89, 180, 0]
+four = [-135, 90, -89, 180, 0]
 five = [0, 180, 90, 90, -90, -90]
 six = []
 seven = [0, -135, 45]
@@ -23,19 +23,27 @@ for row in chisla:
         turtle.forward(dlina)
         turtle.left(90)
         turtle.pendown()
-
+    """elif row == three:
+        turtle.penup()
+        turtle.left(90)
+        turtle.forward(dlina*2)
+        turtle.pendown()
+    """
     for elem in row:
 
         turtle.left(elem)
-        if (row == one and len(one)) or (row == two):
+        if (row == one and elem == 45) or (row == two and (elem == -45 or elem == -135)) \
+                or (row == three and (elem == -135)):
             turtle.forward(sqrt(dlina**2 + dlina**2))
+
         else:
             turtle.forward(dlina)
-    x+=30
+
     turtle.right(90)
     turtle.penup()
-    turtle.forward(30)
+    turtle.goto(x, y)
     turtle.pendown()
+    x += 45
 
 
 
