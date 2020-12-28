e127d7bac716918befc91a420b25e5c781c2b290
VAE <vlad.efr92@yandex.ru>
2020-12-28 17:48:48 +0300

    Написана функция ввода, начата функция сортировки

--- Tkhirianov/contest3.py
+++ Tkhirianov/contest3.py
@@ -3,15 +3,27 @@ inp_data = input()
 
 def input_data(inp_data):
     while inp_data != '#':
+
         A.append(inp_data)
         inp_data = input()
 
 
 def sort(A):
+    for k in A:
+        if len(k) <= 2:
+            A.remove(k)
 
-
+    for i in range(1,len(A)+1):
+        for j in range(1):
+            if j[0] == i[i-1][0]:
+                i[j-1] + i[j]
 
     A.sort(key=lambda x: max((x[-1])))
+    return A
+
+
+
+
 
 input_data(inp_data)
 
