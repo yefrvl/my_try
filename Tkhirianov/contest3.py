77e61f3528493d79474ecd68010eb4b352e73943
VAE <vlad.efr92@yandex.ru>
2021-01-19 16:04:17 +0300

    Написана функция ввода, начата функция сортировки

--- Tkhirianov/contest3.py
+++ Tkhirianov/contest3.py
@@ -1,34 +1,40 @@
 A = []
+B = []
 inp_data = input()
+
 
 def input_data(inp_data):
     while inp_data != '#':
-
         A.append(inp_data.split(' '))
         inp_data = input()
 
 
 def sort(A):
+    amount = 0  # количество студентов
     for k in A:
-        if len(k) <= 2:
+        if len(k) < 2:
+            amount = int(k[0])
             A.remove(k)
 
-    for i in range(1,len(A)+1):
-        for j in range(1):
-            if j[0] == i[i-1][0]:
-                i[j-1] + i[j]
+    p = 1   # счетчик студентов
+    c = 0   # суммарный бал студентов
+    for i in (A):
+        if p <= amount:
+            if int(i[0]) == p:
+                c += int(i[1])
+            else:
+                p+=1
+                B.append(c)
+                c = 0
+    return B
 
-    A.sort(key=lambda x: max((x[-1])))
-    return A
 
-
-
+# A.sort(key=lambda x: max((x[-1])))
+# return A
 
 
 input_data(inp_data)
 
-
-
 sort(A)
 
-print(A)
+print(A, B)
