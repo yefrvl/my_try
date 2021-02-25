f80d49eb6d4b816afb5e54322ae542066298f035
VAE <vlad.efr92@yandex.ru>
2021-02-25 17:03:54 +0300

    Написана функция ввода, начата функция сортировки

--- Tkhirianov/contest3.py
+++ Tkhirianov/contest3.py
@@ -1,7 +1,6 @@
 A = []
 inp_data = input()
 
-
 def input_data(inp_data : int):
     while inp_data != '#':
         A.append(inp_data.split(' '))
@@ -13,38 +12,34 @@ def elements_value(spis_value):
         theSum = theSum + int(element)
     return theSum
 
-
-
 def create_dict(A : list):
     sum_value = {}
     for student_id, student_value in A:
         sum_value.setdefault(str(student_id), [])
         sum_value[str(student_id)].append(student_value)
+    new_sum_value1 = sum_value.copy()
+    new_sum_value = (sort_sum_dict(create_dict_sum_values(sum_value)))
+    print(new_sum_value, new_sum_value1)
 
-    return sum_value
 
 def create_dict_sum_values(sum_value_dict):
     for student_id, student_value in sum_value_dict.items():
         sum_value_dict[student_id] = elements_value(student_value)
     return sum_value_dict
 
+def sort_sum_dict(sum_value_dict):
+    sum_value_list = sum_value_dict.items()
 
-def sort_dict(sum_value_dict):
+    return sorted(sum_value_list, key=lambda x: x[1], reverse=True)
 
-    sum_value_list = []
 
-    for item in sum_value_dict:
-        sum_value_list.append(sum_value_dict.items)
 
-  """  for student_id, student_value in sum_value_dict:
-        str_values = ','.join([str(x) for x in sorted(student_value, reverse=True)])
-        print(f'Студент №{student_id}: {student_value}')"""
 
 
 
 
 
 input_data(inp_data)
-sort_dict(create_dict_sum_values(create_dict(A)))
+create_dict(A)
 
 
