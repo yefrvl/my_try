66b99241b18eff99d4c810b6ac6d358b4c247a6c
VAE <vlad.efr92@yandex.ru>
2021-06-11 16:31:09 +0300

    дописывается функця парсинга, необходимо написать регулярное выражение по которому происходит парсинг ид товара

--- TG bot/main.py
+++ TG bot/main.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
@@ -9,10 +10,18 @@ def get_name_to_look():
 
 def pars_wild(name_to_look):
 
-#    name_to_look = input('Введите наименование искомого товара: ')
+    #name_to_look = input('Введите наименование искомого товара: ')
     url_to_look = f'https://www.wildberries.ru/catalog/0/search.aspx?search={name_to_look}&search=true'
     response = requests.get(url_to_look)
-    soup = BeautifulSoup(response.text, 'lxml')
-    print(soup)
+    rows_data = BeautifulSoup(response.text, 'lxml').find_all('div', class_='dtList i-dtList j-card-item no-left-part')
+    #print(rows_data)
+
+    for element in rows_data:
+        item = element.find_all('div', class_='l_class')
+        id_item = re.findall()
+    print(item)
+
 
 pars_wild(get_name_to_look())
+
+
